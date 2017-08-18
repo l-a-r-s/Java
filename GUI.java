617c2becc46d42acddf0446f3db91ceca7658dbe
fakename <fakemail>
2017-08-18 09:46:42 +0200

    "some new lines ...x)!"wq

--- GUI.java
+++ GUI.java
@@ -4,24 +4,50 @@ import javax.swing.JLabel;
 import javax.swing.JDialog;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 import java.awt.event.*;
+
+import java.util.Random;
+
  
 public class GUI
 {
-     public static void main(String[] args)
-    {
+     private final static String newline = "\n";
+     private static String text = "Some Random Text here";
 
-	class TestActionListener implements ActionListener{
+     public static void main(String[] args)
+     {
+
+	text = "another Text";
+       
+ 
+        JTextField MyInput = new JTextField(15);
+	JTextArea textArea = new JTextArea(5, 20);
+        JScrollPane scrollPane = new JScrollPane(textArea); 
+        
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(100);
+
+	System.out.println(randomInt);
+        
+	
+
+        class TestActionListener implements ActionListener{
+
 		public void actionPerformed(ActionEvent e) {
 		    //System.out.println(e);	
+                    System.out.println(MyInput.getText());
 		    System.out.println("Schaltfläche wurde gedrückt");
 		}           
 	}
 
-        JTextField MyInput = new JTextField("Input here");
- 	System.out.println(MyInput);
-
-	JButton MyButton = new JButton("Abschicken");
+       
+ 	
+	textArea.append(text + newline);
+	
+        JButton MyButton = new JButton("Abschicken");
+            
 
         MyButton.addActionListener(new TestActionListener()); 
           
@@ -33,6 +59,7 @@ public class GUI
         meinFrame.add(new JLabel("Beispiel JLabel"));
 	meinFrame.add(MyInput);
         meinFrame.add(MyButton);
+	meinFrame.add(textArea);
 
         meinFrame.pack();
 
